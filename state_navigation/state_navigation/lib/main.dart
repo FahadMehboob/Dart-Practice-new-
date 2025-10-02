29ad03a611c7b86852d13bd2258b7743b0c8a343
Fahad Mehboob <fahad.fakky@hotmail.com>
2025-10-02 20:24:16 +0500

    learning about context

---
+++ state_navigation/state_navigation/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<String> listData = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: ListView.builder(
+        itemCount: listData.length,
+        itemBuilder: (itemContext, index) => Text("Abc"),
+      ),
+    );
+  }
+}
--- todo app/todo_app_simple/lib/screens/home_screen.dart
+++ todo app/todo_app_simple/lib/screens/home_screen.dart
@@ -22,38 +22,54 @@ class _HomeScreenState extends State<HomeScreen> {
         preferredSize: Size(double.infinity, 250),
         child: CustomAppBar(),
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return Card(
-            color: const Color(0xFF4A3780),
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: ListTile(
-              title: Text(
-                'Task: ${items[index].title}',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+      body: items.isEmpty
+          ? const Center(
+              child: Text(
+                "No Task Today",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              subtitle: Text(
-                items[index].description,
-                style: const TextStyle(color: Colors.white, fontSize: 16),
-              ),
+            )
+          : ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: const Color(0xFF4A3780),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: ListTile(
+                    title: Text(
+                      'Task ${index + 1}: ${items[index].title}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      items[index].description,
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    trailing: GestureDetector(
+                      onTap: () => onDelete(context, index),
+                      child: const Icon(
+                        Icons.delete,
+                        size: 30,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: const Color(0xFF4A3780),
         onPressed: () => onPress(context),
         child: const Icon(
           Icons.add,
-          color: Color(0xFF4A3780),
+          color: Colors.white,
         ),
       ),
     );
@@ -68,6 +84,12 @@ class _HomeScreenState extends State<HomeScreen> {
           items.add(formData);
         });
       }
+    });
+  }
+
+  onDelete(BuildContext context, index) {
+    setState(() {
+      items.removeAt(index);
     });
   }
 }
